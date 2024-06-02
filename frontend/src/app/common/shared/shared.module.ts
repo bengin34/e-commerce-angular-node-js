f0c82ba1a3427a67a7a012263b296fc53b9381a4
bengin34 <becaglar3434@gmail.com>
2024-06-02 10:43:05 +0200

    validation directive

--- frontend/src/app/common/shared/shared.module.ts
+++ frontend/src/app/common/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { ValidDirective } from '../directives/valid.directive';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, FormsModule, RouterModule],
-  exports: [CommonModule, FormsModule, RouterModule],
+  imports: [CommonModule, FormsModule, RouterModule,ValidDirective],
+  exports: [CommonModule, FormsModule, RouterModule,ValidDirective],
 })
 export class SharedModule {}
