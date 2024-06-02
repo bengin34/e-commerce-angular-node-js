448437791273f44a1030d322f595ca8ee4528510
bengin34 <becaglar3434@gmail.com>
2024-06-02 09:38:31 +0200

    FE routes and login html

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,14 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { importProvidersFrom } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
+import { routes } from './app/app.routes';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideHttpClient(),
+    importProvidersFrom(BrowserModule, CommonModule, RouterModule.forRoot(routes)),
+  ],
+}).catch((err) => console.error(err));
