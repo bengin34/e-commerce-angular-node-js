8df7ed838a3cf913375b76f95a1c5e39b043df28
bengin34 <becaglar3434@gmail.com>
2024-06-16 12:17:24 +0200

    Login final

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -5,6 +5,9 @@ import { ToastrService } from 'ngx-toastr';
 import { NgxSpinnerService } from 'ngx-spinner';
 
 import { SharedModule } from '../../common/shared/shared.module';
+import { AuthService } from './service/auth.service';
+import { LoginModel } from './models/login.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -15,15 +18,23 @@ import { SharedModule } from '../../common/shared/shared.module';
 })
 export class LoginComponent {
   constructor(
+    private _auth: AuthService,
     private _toastr: ToastrService,
-    private _spinner: NgxSpinnerService
-  ) {
-   
-  }
+    private _router: Router
+  ) {}
 
   login(form: NgForm) {
     if (form.valid) {
-      console.log(form.value);
+      let model = new LoginModel();
+      model.email = form.controls['email'].value;
+      model.password = form.controls['password'].value;
+
+      this._auth.login(model, (res) => {
+        this._toastr.success('Login is successful');
+        localStorage.setItem('token', res.token);
+        localStorage.setItem('user', JSON.stringify(res.user));
+        this._router.navigateByUrl('/');
+      });
     }
   }
 }
