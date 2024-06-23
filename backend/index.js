88a2d75602a8ff2e8389b0fd42936f120a7f3e4b
bengin34 <becaglar3434@gmail.com>
2024-06-23 13:59:23 +0200

    category module is added

--- backend/index.js
+++ backend/index.js
@@ -6,11 +6,13 @@ const connection = require("./db/db");
 app.use(express.json());
 app.use(cors());
 
-const authRouter = require("./routers/auth.router")
-app.use("/api/auth", authRouter)
+const authRouter = require("./routers/auth.router");
+const categoryRouter = require("./routers/category.router");
 
+app.use("/api/auth", authRouter);
+app.use("/api/category", categoryRouter);
 
-connection()
+connection();
 
 const port = process.env.PORT || 8000;
 app.listen(port, console.log("app is working on http://localhost:8000"));
