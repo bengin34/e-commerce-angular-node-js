3ce8cd798856bf9d738eea369ddc6f105ddfc8ae
bengin34 <becaglar3434@gmail.com>
2024-07-07 22:06:09 +0200

    generic blank component

---
+++ frontend/src/app/common/components/blank/blank.component.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-blank',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './blank.component.html',
+  styleUrl: './blank.component.css'
+})
+export class BlankComponent {
+  @Input() title: string = "";
+  @Input() sectionTitle= "";
+
+}
--- frontend/src/app/common/shared/shared.module.ts
+++ frontend/src/app/common/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { ValidDirective } from '../directives/valid.directive';
+import { BlankComponent } from '../components/blank/blank.component';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, FormsModule, RouterModule,ValidDirective],
-  exports: [CommonModule, FormsModule, RouterModule,ValidDirective],
+  imports: [CommonModule, FormsModule, RouterModule,ValidDirective, BlankComponent],
+  exports: [CommonModule, FormsModule, RouterModule,ValidDirective, BlankComponent],
 })
 export class SharedModule {}
