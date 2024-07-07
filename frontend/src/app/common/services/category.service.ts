4a25642ab0bf06da8529d6bac368b79f170d63ec
bengin34 <becaglar3434@gmail.com>
2024-07-07 16:50:16 +0200

    category service

--- frontend/src/app/common/services/category.service.ts
+++ frontend/src/app/common/services/category.service.ts
@@ -1,20 +1,42 @@
 import { Injectable } from '@angular/core';
 import { GenericHttpService } from './generic-http.service';
 import { CategoryModel } from '../../components/categories/models/category.model';
+import { MessageResponseModel } from '../models/message.response.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CategoryService {
+  constructor(private _http: GenericHttpService) {}
 
-  constructor(
-    private _http: GenericHttpService
-  ) { }
-
-
-  getAll(callBack: (res: CategoryModel[]) => void){
-    this._http.get<CategoryModel[]>("categories", res => callBack(res))
+  getAll(callBack: (res: CategoryModel[]) => void) {
+    this._http.get<CategoryModel[]>('categories', (res) => callBack(res));
   }
 
-  add(name:string, callBack: (res : ))
+  add(name: string, callBack: (res: MessageResponseModel) => void) {
+    let model = { name: name };
+    this._http.post<MessageResponseModel>('categories/add', model, (res) =>
+      callBack(res)
+    );
+  }
+
+  update(
+    _id: string,
+    name: string,
+    callBack: (res: MessageResponseModel) => void
+  ) {
+    let model = { _id: _id, name: name };
+    this._http.post<MessageResponseModel>('categories/update', model, (res) =>
+      callBack(res)
+    );
+  }
+  
+  removeById(_id: string, callBack: (res: MessageResponseModel) => void) {
+    let model = { _id: _id };
+    this._http.post<MessageResponseModel>(
+      'categories/removeById',
+      model,
+      (res) => callBack(res)
+    );
+  }
 }
--- frontend/src/app/components/categories/categories.component.ts
+++ frontend/src/app/components/categories/categories.component.ts
@@ -1,16 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SharedModule } from '../../common/shared/shared.module';
+import { CategoryModel } from './models/category.model';
+import { ToastrService } from 'ngx-toastr';
+import { CategoryService } from '../../common/services/category.service';
 
 @Component({
   selector: 'app-categories',
   standalone: true,
   imports: [SharedModule],
   templateUrl: './categories.component.html',
-  styleUrl: './categories.component.css'
+  styleUrl: './categories.component.css',
 })
-export class CategoriesComponent {
+export class CategoriesComponent implements OnInit {
+  categories: CategoryModel[] = [];
 
+  constructor(
+    private _toastr: ToastrService,
+    private _category: CategoryService
+  ) {}
 
+  ngOnInit(): void {
+    this.getAll();
+  }
 
-  
+  getAll() {
+    this._category.getAll((res) => (this.categories = res));
+  }
 }
