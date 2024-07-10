f064a84aa1e92dab750d8b586c4b72ac100471ec
bengin34 <becaglar3434@gmail.com>
2024-07-10 19:59:30 +0200

    generic table

--- frontend/src/app/common/shared/shared.module.ts
+++ frontend/src/app/common/shared/shared.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { ValidDirective } from '../directives/valid.directive';
 import { BlankComponent } from '../components/blank/blank.component';
+import { TableComponent } from '../components/table/table.component';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, FormsModule, RouterModule,ValidDirective, BlankComponent],
-  exports: [CommonModule, FormsModule, RouterModule,ValidDirective, BlankComponent],
+  imports: [CommonModule, FormsModule, RouterModule,ValidDirective, BlankComponent, TableComponent],
+  exports: [CommonModule, FormsModule, RouterModule,ValidDirective, BlankComponent, TableComponent],
 })
 export class SharedModule {}
